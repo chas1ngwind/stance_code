e48003cc02d0fc6a21392dedc1b1e0d8cd20059b
chas1ngwind <arsenalness@icloud.com>
2020-06-25 14:20:08 +0800

    update cons loss func

--- Das_Bert_cons.py
+++ Das_Bert_cons.py
@@ -230,10 +230,10 @@ class BertForConsistencyCueClassification(BertPreTrainedModel):
 #                 print(loss_ce)
 
                 loss_fct_cos = CosineEmbeddingLoss()
-#                 labels2[labels2==0] = -1
-#                 loss_cos = loss_fct_cos(pooled_output, pooled_outputC, labels2)
+                labels2[labels2==0] = -1
+                loss_cos = loss_fct_cos(pooled_output, pooled_outputC, labels2)
 #                 loss_cos = loss_fct_cos(logits_ce, logits_cos, labels2)
-#                 labels2[labels2==-1] = 0
+                labels2[labels2==-1] = 0
                 
 #                 loss_fct_cos = CosineEmbeddingLoss()
 # #                 print(labels)
@@ -245,7 +245,7 @@ class BertForConsistencyCueClassification(BertPreTrainedModel):
         
         
 # #                 loss_cos = loss_fct_cos(logits_ce, logits_cos, labels2)
-                loss_cos = loss_fct_ce(logits_cos.view(-1, self.num_labels), labels2.view(-1))
+#                 loss_cos = loss_fct_ce(logits_cos.view(-1, self.num_labels), labels2.view(-1))
 #                 print('loss_cos:')
 #                 print(loss_cos)
             
