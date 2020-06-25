290871ce31338f0ef7298879f62b9624d3575b19
chas1ngwind <arsenalness@icloud.com>
2020-06-25 15:46:56 +0800

    fix loss func

--- Das_Bert_cons.py
+++ Das_Bert_cons.py
@@ -249,7 +249,7 @@ class BertForConsistencyCueClassification(BertPreTrainedModel):
 #                 print('loss_cos:')
 #                 print(loss_cos)
             
-                loss = loss_cos+loss_ce
+                loss = loss_cos
 #                 print('final loss:')
 #                 print(loss)
 #                 logits = self.classifier(loss)
@@ -329,22 +329,22 @@ model.to(device)
 # Get all of the model's parameters as a list of tuples.
 params = list(model.named_parameters())
 
-print('The BERT model has {:} different named parameters.\n'.format(len(params)))
+# print('The BERT model has {:} different named parameters.\n'.format(len(params)))
 
-print('==== Embedding Layer ====\n')
+# print('==== Embedding Layer ====\n')
 
-for p in params[0:5]:
-    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+# for p in params[0:5]:
+#     print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
 
-print('\n==== First Transformer ====\n')
+# print('\n==== First Transformer ====\n')
 
-for p in params[5:21]:
-    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+# for p in params[5:21]:
+#     print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
     
-print('\n==== Output Layer ====\n')
+# print('\n==== Output Layer ====\n')
 
-for p in params[-4:]:
-    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+# for p in params[-4:]:
+#     print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
 
 
 # In[14]:
