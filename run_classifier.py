169000f27069e9772ebb8cc014c45bac444e152d
chas1ngwind <arsenalness@icloud.com>
2020-07-13 02:39:26 +0800

    fix bug

--- run_classifier.py
+++ run_classifier.py
@@ -1239,15 +1239,15 @@ def convert_opp_claims_to_features(examples, label_list, max_seq_length, tokeniz
                               segment_ids=segment_ids,
                               label_id=label_id))
             if ex_index < 5:
-            logger.info("*** Opposite Claim Example ***")
-            logger.info("guid: %s" % (example.guid))
-            logger.info("tokens: %s" % " ".join(
-                    [str(x) for x in tokens]))
-            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-            logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-            logger.info(
-                    "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-            logger.info("label: %s (id = %d)" % (example.label, label_id))
+                logger.info("*** Opposite Claim Example ***")
+                logger.info("guid: %s" % (example.guid))
+                logger.info("tokens: %s" % " ".join(
+                        [str(x) for x in tokens]))
+                logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+                logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+                logger.info(
+                        "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+                logger.info("label: %s (id = %d)" % (example.label, label_id))
             
         else:
             input_ids=None
