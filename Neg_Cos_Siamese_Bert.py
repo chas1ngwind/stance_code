9679a254383af291d2201371d30ffe8dfb6d9c28
chas1ngwind <arsenalness@icloud.com>
2020-07-15 16:41:15 +0800

    minor fix

--- Neg_Cos_Siamese_Bert.py
+++ Neg_Cos_Siamese_Bert.py
@@ -632,7 +632,7 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
         
         new_test_df.to_csv(os.path.join(data_dir, "new_dev.tsv"),sep='\t',index=False)
 
-        eval_examples = processor.get_test_examples(data_dir)
+        eval_examples = processor.get_dev_examples(data_dir)
 #         eval_examples = processor.get_dev_examples(data_dir)
         claim_features = convert_claims_to_features(eval_examples, label_list, max_seq_length, tokenizer)
         eval_features = convert_pers_to_features(eval_examples, label_list, max_seq_length, tokenizer)
