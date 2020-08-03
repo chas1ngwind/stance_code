bc4170c7f0778a897bca2aa07ed4581dc59a10f5
chas1ngwind <arsenalness@icloud.com>
2020-08-03 14:31:58 +0800

    add counter

--- Neg_Cos_Siamese_Bert.py
+++ Neg_Cos_Siamese_Bert.py
@@ -680,7 +680,11 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
         opp_claims_segment_ids = torch.tensor([f.segment_ids for f in opposite_claim_features], dtype=torch.long)
         opp_claims_label_ids = torch.tensor([f.label_id for f in opposite_claim_features], dtype=torch.long)
         
+        logger.info("%d%d%d%d", len(pers_input_ids),len(claims_input_ids),len(opp_pers_input_ids),len(opp_claims_input_ids))
+        
         eval_data = TensorDataset(pers_input_ids, pers_input_mask, pers_segment_ids, pers_label_ids, claims_input_ids, claims_input_mask, claims_segment_ids, claims_label_ids, opp_pers_input_ids, opp_pers_input_mask, opp_pers_segment_ids, opp_pers_label_ids, opp_claims_input_ids, opp_claims_input_mask, opp_claims_segment_ids, opp_claims_label_ids)
+        
+        logger.info(eval_data)
         # Run prediction for full data
 #         eval_sampler = SequentialSampler(eval_data)
         eval_sampler = SequentialSampler(eval_data)
