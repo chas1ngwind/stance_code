473388d78e6c02f0b7e12ce5a9bb577fe3256770
chas1ngwind <arsenalness@icloud.com>
2020-08-12 15:39:32 +0800

    neg model to train mode

--- Neg_Cos_Siamese_Bert.py
+++ Neg_Cos_Siamese_Bert.py
@@ -328,8 +328,8 @@ class BertForConsistencyCueClassification(BertPreTrainedModel):
 import csv
 from pytorch_pretrained_bert.file_utils import PYTORCH_PRETRAINED_BERT_CACHE
 def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
-                   output_dir=None, max_seq_length=80, do_train=False, do_eval=False, do_lower_case=False,
-                   train_batch_size=24, eval_batch_size=8, learning_rate=5e-5, num_train_epochs=5,
+                   output_dir=None, max_seq_length=128, do_train=False, do_eval=False, do_lower_case=False,
+                   train_batch_size=24, eval_batch_size=8, learning_rate=1e-5, num_train_epochs=15,
                    warmup_proportion=0.1,no_cuda=False, local_rank=-1, seed=42, gradient_accumulation_steps=1,
                    optimize_on_cpu=False, fp16=False, loss_scale=128, saved_model=""):
     
@@ -635,7 +635,7 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
                     model.zero_grad()
                     global_step += 1
 
-        torch.save(model.state_dict(), output_dir +"neg_siamese_bert_epoch5.pth")
+        torch.save(model.state_dict(), output_dir +"10505025_1e5_neg_siamese_bert_epoch5.pth")
 
 
     if do_eval and (local_rank == -1 or torch.distributed.get_rank() == 0):
@@ -813,8 +813,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 #                   'loss': tr_loss/nb_tr_steps
                   }
 
-        output_eval_file = os.path.join(output_dir, "10505025_onnew_neg_siamese_bert_epoch5_eval_results.txt")
-        output_raw_score = os.path.join(output_dir, "10505025_onnew_neg_siamese_bert_epoch5_raw_score.csv")
+        output_eval_file = os.path.join(output_dir, "10505025_1e5_neg_siamese_bert_epoch15_eval_results.txt")
+        output_raw_score = os.path.join(output_dir, "10505025_1e5_neg_siamese_bert_epoch15_raw_score.csv")
         with open(output_eval_file, "w") as writer:
             logger.info("***** Eval results *****")
             for key in sorted(result.keys()):
@@ -849,8 +849,8 @@ def experiments():
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/ibmcs/"
     
     
-#     data_dir_output = "/var/scratch/syg340/project/cos_siamese_models/siamese_ibmcs/"
-    data_dir_output = "/var/scratch/syg340/project/stance_code/Evaluation/319/"
+    data_dir_output = "/var/scratch/syg340/project/cos_siamese_models/"
+#     data_dir_output = "/var/scratch/syg340/project/stance_code/Evaluation/319/"
     train_and_test(data_dir=data_dir, do_train=True, do_eval=False, output_dir=data_dir_output,task_name="neg")
 
 
@@ -859,7 +859,7 @@ def experiments():
 
 def evaluation_with_pretrained():
 #     bert_model = "/var/scratch/syg340/project/cos_siamese_models/319cos/319_cos_camimu_siamese_bert_epoch5.pth"
-    bert_model = "/var/scratch/syg340/project/neg_models/neg_siamese_bert_epoch5.pth"
+    bert_model = "/var/scratch/syg340/project/neg_models/10505025_1e5_neg_siamese_bert_epoch5.pth"
     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/"
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/ibmcs/"
 
@@ -871,8 +871,8 @@ def evaluation_with_pretrained():
 
 
 if __name__ == "__main__":
-#     experiments()
-    evaluation_with_pretrained()
+    experiments()
+#     evaluation_with_pretrained()
 
 
 # In[ ]:
