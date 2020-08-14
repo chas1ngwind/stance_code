b5e56560f7bdc75a7715baaf4f18c6475703ea15
chas1ngwind <arsenalness@icloud.com>
2020-08-14 13:12:17 +0800

    modify model names for loss neg model

--- Loss_Neg_Cos_Siamese_Bert.py
+++ Loss_Neg_Cos_Siamese_Bert.py
@@ -323,8 +323,8 @@ class BertForConsistencyCueClassification(BertPreTrainedModel):
                 
 #                 logger.info('loss_cos:')
 #                 logger.info(loss_cos)
-                loss_sim = loss_cos+(0.33*loss_cos2)+(0.33*loss_cos3)+(0.33*loss_cos4)
-                loss = loss_ce+loss_cos
+                loss_sim = loss_cos+(0.5*loss_cos2)+(0.5*loss_cos3)+(0.25*loss_cos4)
+                loss = loss_ce+loss_sim
 #                 logger.info('final loss:')
 #                 logger.info(loss)
                 
@@ -648,7 +648,7 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
                     model.zero_grad()
                     global_step += 1
 
-        torch.save(model.state_dict(), output_dir +"10505025_1e5_neg_siamese_bert_epoch5.pth")
+        torch.save(model.state_dict(), output_dir +"fuse_cosloss_10505025_1e5_neg_siamese_bert_epoch15.pth")
 
 
     if do_eval and (local_rank == -1 or torch.distributed.get_rank() == 0):
@@ -842,8 +842,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 #                   'loss': tr_loss/nb_tr_steps
                   }
 
-        output_eval_file = os.path.join(output_dir, "10505025_1e5_neg_siamese_bert_epoch15_eval_results.txt")
-        output_raw_score = os.path.join(output_dir, "10505025_1e5_neg_siamese_bert_epoch15_raw_score.csv")
+        output_eval_file = os.path.join(output_dir, "fuse_cosloss_10505025_1e5_neg_siamese_bert_epoch15_eval_results.txt")
+        output_raw_score = os.path.join(output_dir, "fuse_cosloss_10505025_1e5_neg_siamese_bert_epoch15_raw_score.csv")
         
         logger.info(classification_report(gold_labels, predicted_labels, target_names=label_list, digits=4))
         with open(output_eval_file, "w") as writer:
@@ -897,7 +897,7 @@ def experiments():
 
 def evaluation_with_pretrained():
 #     bert_model = "/var/scratch/syg340/project/cos_siamese_models/319cos/319_cos_camimu_siamese_bert_epoch5.pth"
-    bert_model = "/var/scratch/syg340/project/cos_siamese_models/10505025_1e5_neg_siamese_bert_epoch5.pth"
+    bert_model = "/var/scratch/syg340/project/cos_siamese_models/fuse_cosloss_10505025_1e5_neg_siamese_bert_epoch15.pth"
     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/"
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/ibmcs/"
 
