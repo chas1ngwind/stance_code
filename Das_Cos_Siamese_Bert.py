cb53b9d8bb01ff036840281b1e90dc76710855b8
chas1ngwind <arsenalness@icloud.com>
2020-09-08 16:17:01 +0800

    eval on new neg dataset

--- Das_Cos_Siamese_Bert.py
+++ Das_Cos_Siamese_Bert.py
@@ -500,7 +500,7 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
                     model.zero_grad()
                     global_step += 1
             print("\nLoss: {}\n".format(tr_loss / nb_tr_steps))
-        torch.save(model.state_dict(), output_dir + "bert_sia_cos_bs24_lr2e_5_epoch30.pth")
+        torch.save(model.state_dict(), output_dir + "new_neg_bert_sia_cos_bs24_lr2e_5_epoch30.pth")
 
 
     if do_eval and (local_rank == -1 or torch.distributed.get_rank() == 0):
@@ -637,8 +637,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 #                   'loss': tr_loss/nb_tr_steps
                   }
 
-        output_eval_file = os.path.join(output_dir, "bert_sia_cos_bs24_lr2e_5_epoch30_eval_results.txt")
-        output_raw_score = os.path.join(output_dir, "bert_sia_cos_bs24_lr2e_5_epoch30_raw_score.csv")
+        output_eval_file = os.path.join(output_dir, "new_neg_bert_sia_cos_bs24_lr2e_5_epoch30_eval_results.txt")
+        output_raw_score = os.path.join(output_dir, "new_neg_bert_sia_cos_bs24_lr2e_5_epoch30_raw_score.csv")
         logger.info(classification_report(gold_labels, predicted_labels, target_names=label_list, digits=4))
         with open(output_eval_file, "w") as writer:
             logger.info("***** Eval results *****")
@@ -671,7 +671,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 def experiments():
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset"
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/stancy/"
-    data_dir = "/var/scratch/syg340/project/stance_code/Dataset/"
+#     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/"
+    data_dir = "/var/scratch/syg340/project/stance_code/Dataset/new_neg/"
     
     
     data_dir_output = "/var/scratch/syg340/project/cos_siamese_models/"
