3f9a040819beb71c72a8b01d0aedf0c8786145f6
chas1ngwind <arsenalness@icloud.com>
2020-11-04 17:08:15 +0800

    check argv fix

--- Individual_analysis_Margin_Triplet_Neg_Cos_Siamese_Bert.py
+++ Individual_analysis_Margin_Triplet_Neg_Cos_Siamese_Bert.py
@@ -722,11 +722,12 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 #         new_dev_df.to_csv(os.path.join(data_dir, "new_dev.tsv"),sep='\t',index=False)
 
 #         eval_examples = processor.get_test_examples(data_dir)
-        eval_examples = processor.get_test_from_command(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
         print(sys.argv[1])
         print(sys.argv[2])
         print(sys.argv[3])
         print(sys.argv[4])
+        eval_examples = processor.get_test_from_command(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        
 #         eval_examples = processor.get_dev_examples(data_dir)
         claim_features = convert_claims_to_features(eval_examples, label_list, max_seq_length, tokenizer)
         eval_features = convert_pers_to_features(eval_examples, label_list, max_seq_length, tokenizer)
