14017444a663d85bc89cff55923ae8a065766a7d
chas1ngwind <arsenalness@icloud.com>
2020-12-10 20:39:56 +0800

    rollback to get distance logitis results on test set

--- Distance_Margin_Triplet_Neg_Cos_Siamese_Bert.py
+++ Distance_Margin_Triplet_Neg_Cos_Siamese_Bert.py
@@ -707,8 +707,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
         
 #         new_dev_df.to_csv(os.path.join(data_dir, "new_dev.tsv"),sep='\t',index=False)
 
-#         eval_examples = processor.get_test_examples(data_dir)
-        eval_examples = processor.get_train_examples(data_dir)
+        eval_examples = processor.get_test_examples(data_dir)
+#         eval_examples = processor.get_train_examples(data_dir)
 #         eval_examples = processor.get_dev_examples(data_dir)
         claim_features = convert_claims_to_features(eval_examples, label_list, max_seq_length, tokenizer)
         eval_features = convert_pers_to_features(eval_examples, label_list, max_seq_length, tokenizer)
@@ -918,8 +918,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 #                   'loss': tr_loss/nb_tr_steps
                   }
 
-        output_eval_file = os.path.join(output_dir, "train_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_eval_results.txt")
-        output_raw_score = os.path.join(output_dir, "train_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_raw_score.csv")
+        output_eval_file = os.path.join(output_dir, "test_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_eval_results.txt")
+        output_raw_score = os.path.join(output_dir, "test_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_raw_score.csv")
         
 #         logger.info(classification_report(gold_labels, predicted_labels, target_names=label_list, digits=4))
         with open(output_eval_file, "w") as writer:
