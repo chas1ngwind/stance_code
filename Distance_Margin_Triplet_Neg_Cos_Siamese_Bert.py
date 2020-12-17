4c7459184b0154e1fd622bca038a4952e41b6658
chas1ngwind <arsenalness@icloud.com>
2020-12-17 15:03:30 +0800

    get dev results

--- Distance_Margin_Triplet_Neg_Cos_Siamese_Bert.py
+++ Distance_Margin_Triplet_Neg_Cos_Siamese_Bert.py
@@ -707,9 +707,9 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
         
 #         new_dev_df.to_csv(os.path.join(data_dir, "new_dev.tsv"),sep='\t',index=False)
 
-        eval_examples = processor.get_test_examples(data_dir)
+#         eval_examples = processor.get_test_examples(data_dir)
 #         eval_examples = processor.get_train_examples(data_dir)
-#         eval_examples = processor.get_dev_examples(data_dir)
+        eval_examples = processor.get_dev_examples(data_dir)
         claim_features = convert_claims_to_features(eval_examples, label_list, max_seq_length, tokenizer)
         eval_features = convert_pers_to_features(eval_examples, label_list, max_seq_length, tokenizer)
         
@@ -918,8 +918,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 #                   'loss': tr_loss/nb_tr_steps
                   }
 
-        output_eval_file = os.path.join(output_dir, "train_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_eval_results.txt")
-        output_raw_score = os.path.join(output_dir, "train_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_raw_score.csv")
+        output_eval_file = os.path.join(output_dir, "dev_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_eval_results.txt")
+        output_raw_score = os.path.join(output_dir, "dev_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15_raw_score.csv")
         
 #         logger.info(classification_report(gold_labels, predicted_labels, target_names=label_list, digits=4))
         with open(output_eval_file, "w") as writer:
@@ -977,7 +977,8 @@ def experiments():
 def evaluation_with_pretrained():
 #     bert_model = "/var/scratch/syg340/project/cos_siamese_models/319cos/319_cos_camimu_siamese_bert_epoch5.pth"
     bert_model = "/var/scratch/syg340/project/triplet_siamese_models/concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15.pth"
-    data_dir = "/var/scratch/syg340/project/stance_code/Dataset/tri_train/"
+    data_dir = "/var/scratch/syg340/project/stance_code/Dataset/"
+#     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/tri_train/"
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/ibmcs/"
 
     data_dir_output = "/var/scratch/syg340/project/stance_code/Evaluation/bert_dummy_output/"
