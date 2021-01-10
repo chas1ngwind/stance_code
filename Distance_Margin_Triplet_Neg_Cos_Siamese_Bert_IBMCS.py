61d3470c846c506e864032b594d58b5e9a92964f
chas1ngwind <arsenalness@icloud.com>
2021-01-11 00:06:03 +0800

    typo

--- Distance_Margin_Triplet_Neg_Cos_Siamese_Bert_IBMCS.py
+++ Distance_Margin_Triplet_Neg_Cos_Siamese_Bert_IBMCS.py
@@ -981,7 +981,7 @@ def experiments():
 def evaluation_with_pretrained():
 #     bert_model = "/var/scratch/syg340/project/cos_siamese_models/319cos/319_cos_camimu_siamese_bert_epoch5.pth"
 #     bert_model = "/var/scratch/syg340/project/triplet_siamese_models/concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15.pth"
-    bert_model = "/var/scratch/syg340/project/triplet_siamese_models/ibmcs_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15.pth"
+    bert_model = "/var/scratch/syg340/project/triplet_siamese_models/ibmcs_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch15.pth"
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/"
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/tri_train/"
     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/ibmcs/"
