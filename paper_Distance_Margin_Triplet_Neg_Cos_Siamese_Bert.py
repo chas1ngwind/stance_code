f3049226d48a422a16fdc5c876bb4f76705e3944
chas1ngwind <arsenalness@icloud.com>
2021-03-27 23:12:53 +0800

    run time record eval tribrid

--- paper_Distance_Margin_Triplet_Neg_Cos_Siamese_Bert.py
+++ paper_Distance_Margin_Triplet_Neg_Cos_Siamese_Bert.py
@@ -918,8 +918,8 @@ def train_and_test(data_dir, bert_model="bert-base-uncased", task_name=None,
 #                   'loss': tr_loss/nb_tr_steps
                   }
 
-        output_eval_file = os.path.join(output_dir,"train_on_per_eval_on_ibmcs_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch25_eval_results.txt")
-        output_raw_score = os.path.join(output_dir,"train_on_per_eval_on_ibmcs_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch25_raw_score.csv")
+        output_eval_file = os.path.join(output_dir,"time_pers_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch25_eval_results.txt")
+        output_raw_score = os.path.join(output_dir,"time_pers_distance_concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch25_raw_score.csv")
         
 #         logger.info(classification_report(gold_labels, predicted_labels, target_names=label_list, digits=4))
         with open(output_eval_file, "w") as writer:
@@ -977,9 +977,9 @@ def experiments():
 def evaluation_with_pretrained():
 #     bert_model = "/var/scratch/syg340/project/cos_siamese_models/319cos/319_cos_camimu_siamese_bert_epoch5.pth"
     bert_model = "/var/scratch/syg340/project/triplet_siamese_models/concat_margin1_costriplet_cos_siamese_bs24_lr2e_5_epoch25.pth"
-#     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/paper/"
+    data_dir = "/var/scratch/syg340/project/stance_code/Dataset/paper/"
 #     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/tri_train/"
-    data_dir = "/var/scratch/syg340/project/stance_code/Dataset/ibmcs/"
+#     data_dir = "/var/scratch/syg340/project/stance_code/Dataset/ibmcs/"
 
     data_dir_output = "/var/scratch/syg340/project/stance_code/Evaluation/paper_output/"
     train_and_test(data_dir=data_dir, do_train=False, do_eval=True, output_dir=data_dir_output,task_name="tri",saved_model=bert_model)
@@ -989,8 +989,8 @@ def evaluation_with_pretrained():
 
 
 if __name__ == "__main__":
-#     experiments()
-    evaluation_with_pretrained()
+    experiments()
+#     evaluation_with_pretrained()
 #
 
 # In[ ]:
